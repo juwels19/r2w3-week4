b5664b09acacb855cfcf9f87b0ddc1a1fa0eb532
Julian C <julian.curalli27@gmail.com>
2022-11-04 18:03:26 -0400

    Tutorial done and challenge 1 partially done

---
+++ components/nftCard.jsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { useState } from "react";
+
+export const NFTCard = ({ nft }) => {
+
+    const [copyToClipboard, setCopyToClipboard] = useState(false);
+
+    function truncateString(string, bp1, bp2) {
+        if (bp2 == 0) {
+            return string.slice(0, bp1) + "...";    
+        }
+        return string.slice(0, bp1) + "..." + string.slice(-bp2);
+    }
+
+    return (
+        <div className="w-1/4 flex flex-col ">
+        <div className="rounded-md">
+            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                <p className="text-gray-600">ID: {Number(nft.id.tokenId)}</p>
+                <div className="flex">
+                    <div className="flex-none cursor-pointer" onClick={
+                        () => {
+                            navigator.clipboard.writeText(nft.contract.address)
+                            setCopyToClipboard(true);
+                        }
+                    }>
+                        <Image 
+                            src="/copy-symbol.png"
+                            width="20"
+                            height="20"
+                        />
+                    </div>
+                    <div className="flex-auto pl-5">
+                        <p className="text-gray-600" >{nft.contract.address}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">{truncateString(nft.description, 100, 0)}</p>
+            </div>
+        </div>
+
+    </div>
+    )
+}
